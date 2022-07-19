6fb2910fdca75d5343149cc3aabd3b3f9bd7575c
LucasArmanelli <luc.armanelli@gmail.com>
2022-07-19 16:05:40 -0300

    useRef 2

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import UseRefExemple1 from "./components/UseRefExemple1";
+import UseRefExemple2 from "./components/UseRefExemple2";
 
 function App() {
   return (
     <div className='container mt-5'>
       <UseRefExemple1 />
+      <UseRefExemple2 />
     </div>
   );
 }
